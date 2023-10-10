b26527cfa33688b06066227d682b510bc4127fa8
Rick <rick1304.25@gmail.com>
2023-10-10 10:59:56 -0600

    Declaración estática de lsa torres, e impresión de pasos

--- src/main/java/org/example/Hanoi.java
+++ src/main/java/org/example/Hanoi.java
@@ -3,9 +3,21 @@ package org.example;
 import java.util.Stack;
 
 public class Hanoi {
+    // Se declara un arreglo de stacks (Torres de Hanói)
+    public static Stack<Integer>[] stacks = new Stack[3];
+
+    public static void printAll() {
+        for (Stack<Integer> stack: stacks) {
+            System.out.println(stack);
+        }
+        System.out.println("-----------------------");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static void main(String[] args) {
-        // Se declara un arreglo de stacks (Torres de Hanói)
-        Stack<Integer>[] stacks = new Stack[3];
         // Se declaran las torres
         stacks[0] = new Stack<>();
         stacks[1] = new Stack<>();
@@ -23,8 +35,6 @@ public class Hanoi {
         // Se imprimen las torres
         System.out.printf("%s\n%s\n%s\n-------------------------------\n", stacks[0], stacks[1], stacks[2]);
         hanoi(stacks[0].size(), stacks[0], stacks[1], stacks[2]);
-        // Se vuélen a imprimir las torres después de ordenarla
-        System.out.printf("%s\n%s\n%s\n-------------------------------\n", stacks[0], stacks[1], stacks[2]);
     }
 
     /**
@@ -39,12 +49,14 @@ public class Hanoi {
         if (n == 1) {
             // Se mueve de la primera torre a la segunda
             stackTwo.push(stackOne.pop());
+            printAll();
         // Si no
         } else {
             // Se mueven todos los discos (menos el base) hacia la torre auxiliar
             hanoi(n - 1, stackOne, stackThree, stackTwo);
             // Se mueve la base a la torre deseada (2da torre)
             stackTwo.push(stackOne.pop());
+            printAll();
             // Por último, se mueven los discos de la torre auxiliar a la torre deseada (2da torre)
             hanoi(n - 1, stackThree, stackTwo, stackOne);
         }
